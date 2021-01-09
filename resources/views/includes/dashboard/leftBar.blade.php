16eea64042146407cd1043a05ca9b79ea8e3f4e5
michele <michele@MacbookPro-di-michele.local>
2021-01-09 18:02:11 +0100

    Edited Layout

--- resources/views/includes/dashboard/leftBar.blade.php
+++ resources/views/includes/dashboard/leftBar.blade.php
@@ -1,4 +1,4 @@
-<div class="list-group">
+<div class="list-group mb-3">
     <div class="list-group-item"><a href="{{route('home')}}">Menage Links</a></div>
     <div class="list-group-item"><a href="{{route('manageProfile')}}">Profilo</a></div>
 </div>
